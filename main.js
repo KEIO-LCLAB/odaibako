821d83e550b5ceab3022e9c35fb5cd2eb5baad96
tokkyo13 <the.user.is.tokkyo13@gmail.com>
2021-08-30 14:30:01 +0900

    お題を出し切ったらもう１周始めるようにした

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // すでに選んだお題のインデックスを保存する配列
-const used_topics = [];
+let used_topics = [];
 
 // 指定した範囲の整数をランダムに返す
 // 0は含み，maxは含まない
@@ -8,8 +8,9 @@ function getRandomInt(max) {
     const num = Math.floor(Math.random() * max);
 
     if (used_topics.length == max) {
-        // すでに選んだお題の数が最大値に等しければ-1を返し終了を知らせる
-        return -1;
+        // すでに選んだお題の数が最大値に等しければused_topicsをリセットする
+        used_topics = [];
+        return getRandomInt(max);
     } else if (used_topics.includes(num)) {
         // 生成した整数がすでに選んだものであればもう一度選びなおす
         return getRandomInt(max);
@@ -31,10 +32,5 @@ function dice() {
     const index = getRandomInt(topics.length);
 
     // 生成されたインデックスのお題を表示する
-    // -1 が帰ってきたらお題が出尽くしている
-    if (index < 0) {
-        document.getElementById("topic").textContent = "終";
-    } else {
-        document.getElementById("topic").textContent = topics[index];
-    }
+    document.getElementById("topic").textContent = topics[index];
 }
